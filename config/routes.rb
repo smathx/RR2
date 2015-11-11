526cff5ef6f104fbe7cf3c2450b08430bfa219a7
Stephen Matthews <smath@opal7.fsnet.co.uk>
2015-11-11 12:01:12 +0000

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  root to: "books#index"
+  
+  resources :books do
+    resources :notes, only: [:create, :destroy]
+  end
+
+  resources :sessions, only: [:new, :create, :destroy]
+
+  get "/login" => "sessions#new", as: "login"
+  delete "/logout" => "sessions#destroy", as: "logout"
+end
